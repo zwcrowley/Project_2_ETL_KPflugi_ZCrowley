55d55657a9ef37d12d3edf9db1e685bf8e358180
@zwcrowley-qczvvO <zwcrowley@gmail.com>
2022-12-20 21:21:31 -0600

    db and tables created in sql

--- table_schema_Crowlugi.sql
+++ table_schema_Crowlugi.sql
@@ -1,4 +1,4 @@
--- Project 2: Crowlugi Resort
+-- Project 2 ETL: Crowlugi Resort
 -- By: Kathleen Pflugi and Zack Crowley
 -- Table Schema for loading data into db in postgreSQL
 
@@ -16,86 +16,81 @@ CREATE TABLE country (
     city_ID INTEGER NOT NULL,
     city VARCHAR(255),
     country_ID INTEGER NOT NULL,
+    PRIMARY KEY (city_ID, country_ID)
+);
+-- Import data from new_country_df using sqlalchemy.
+
+-- Create happiness table:
+CREATE TABLE happiness (
+    country_ID INTEGER NOT NULL,
+    overall_rank INTEGER NOT NULL,
+    country VARCHAR(255),
+    score FLOAT NOT NULL,
+    generosity  FLOAT NOT NULL,
+    perceptions_of_corruption  FLOAT NOT NULL,
+    PRIMARY KEY (country_ID)
+);
+-- Import data from new_happiness_df using sqlalchemy.
+
+-- Create cities table:
+CREATE TABLE cities (
+    city_ID INTEGER NOT NULL,
+    city VARCHAR(255),
+    lat FLOAT NOT NULL,
+    lng FLOAT NOT NULL,
+    population  FLOAT NOT NULL,
     PRIMARY KEY (city_ID)
 );
--- Import data from country_df using sqlalchemy.
------------------------***************
 
-
--- Create dept_emp table with both columns as pk:
-CREATE TABLE dept_emp (
-    emp_no INTEGER NOT NULL,
-    dept_no VARCHAR(255),
-    PRIMARY KEY(emp_no, dept_no)
+-- Create airport table:
+CREATE TABLE airport (
+    city_ID INTEGER NOT NULL,
+    airport_name VARCHAR(255),
+    airport_address VARCHAR(255),
+    airport_rating FLOAT NOT NULL,
+    PRIMARY KEY (city_ID)
 );
--- Import dept_emp.csv using pgAdmin GUI.
+-- Import data from new_airport_df using sqlalchemy.
 
--- Create dept_manager table with both columns as pk:
-CREATE TABLE dept_manager (
-    dept_no VARCHAR(255),
-    emp_no INTEGER NOT NULL,
-    PRIMARY KEY(dept_no, emp_no)
+-- Create beaches table:
+CREATE TABLE beaches (
+    city_ID INTEGER NOT NULL,
+    beach_name VARCHAR(255),
+    beach_address VARCHAR(255),
+    beach_rating FLOAT NOT NULL,
+    PRIMARY KEY (city_ID)
 );
--- Import dept_manager.csv using pgAdmin GUI.
+-- Import data from new_beaches_df using sqlalchemy.
 
--- Create employees table:
-CREATE TABLE employees (
-    emp_no INTEGER NOT NULL,
-    emp_title_id VARCHAR(255),
-    birth_date DATE NOT NULL,
-    first_name VARCHAR(255),
-    last_name VARCHAR(255),
-    sex VARCHAR(255),
-    hire_date DATE NOT NULL,
-    PRIMARY KEY (emp_no)
+-- Create weather table:
+CREATE TABLE weather (
+    city_ID INTEGER NOT NULL,
+    min_temp FLOAT NOT NULL,
+    max_temp  FLOAT NOT NULL,
+    humidity INTEGER NOT NULL,
+    cloudiness INTEGER NOT NULL,
+    PRIMARY KEY (city_ID)
 );
--- Import employees.csv using pgAdmin GUI.
+-- Import data from new_weather_df using sqlalchemy.
 
--- Create salaries table:
-CREATE TABLE salaries (
-    emp_no INTEGER NOT NULL,
-    salary INTEGER NOT NULL,
-    PRIMARY KEY (emp_no)
-);
--- Import salaries.csv using pgAdmin GUI.
-
--- Create titles table:
-CREATE TABLE titles (
-    title_id VARCHAR(255),
-    title VARCHAR(255),
-    PRIMARY KEY (title_id)
-);
--- Import titles.csv using pgAdmin GUI.
-
+-----------
+**************** NEED TO FINISH ADDING FOREIGN KEYS
 -- Add foreign keys to all of the tables now that all the tables are added:
-ALTER TABLE dept_emp ADD CONSTRAINT fk_dept_emp_emp_no FOREIGN KEY(emp_no)
-REFERENCES employees (emp_no);
-
-ALTER TABLE dept_emp ADD CONSTRAINT fk_dept_emp_dept_no FOREIGN KEY(dept_no)
-REFERENCES departments (dept_no);
-
-ALTER TABLE dept_manager ADD CONSTRAINT fk_dept_manager_dept_no FOREIGN KEY(dept_no)
-REFERENCES departments (dept_no);
-
-ALTER TABLE dept_manager ADD CONSTRAINT fk_dept_manager_emp_no FOREIGN KEY(emp_no)
-REFERENCES salaries (emp_no);
-
-ALTER TABLE employees ADD CONSTRAINT fk_employees_emp_no FOREIGN KEY(emp_no)
-REFERENCES salaries (emp_no);
-
-ALTER TABLE employees ADD CONSTRAINT fk_employees_emp_title_id FOREIGN KEY(emp_title_id)
-REFERENCES titles (title_id);
+ALTER TABLE country ADD CONSTRAINT fk_country_happiness FOREIGN KEY (country_ID)
+REFERENCES happiness (country_ID);
+****************
+-----------
 
 --Check all tables:
-SELECT * FROM departments;
+SELECT * FROM country;
 
-SELECT * FROM dept_emp;
+SELECT * FROM happiness;
 
-SELECT * FROM dept_manager;
+SELECT * FROM cities;
 
-SELECT * FROM employees;
+SELECT * FROM airport;
 
-SELECT * FROM salaries;
+SELECT * FROM beaches;
 
-SELECT * FROM titles;
+SELECT * FROM weather;
 
